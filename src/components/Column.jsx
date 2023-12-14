328c243ee1a818c6ef29118976e9f8e3b0c77677
T1t45 <titasbartulis@gmail.com>
2023-12-14 21:32:57 +0200

    Implemented the moving of a task

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -3,19 +3,39 @@ import { useState } from "react";
 import { useStore } from "../store";
 import Task from "./Task";
 import { shallow } from "zustand/shallow";
+import classNames from "classnames";
 
 export default function Column({ state }) {
   const [text, setText] = useState("");
   const [open, setOpen] = useState(false);
+  const [drop, setDrop] = useState(false);
 
   const tasks = useStore(
     (store) => store.tasks.filter((task) => task.state === state),
     shallow
   );
   const addTask = useStore((store) => store.addTask);
+  const setDraggedTask = useStore((store) => store.setDraggedTask);
+  const draggedTask = useStore((store) => store.draggedTask);
+  const moveTask = useStore((store) => store.moveTask);
 
   return (
-    <div className="column">
+    <div
+      className={classNames("column", { drop: drop })}
+      onDragOver={(e) => {
+        setDrop(true);
+        e.preventDefault();
+      }}
+      onDragLeave={(e) => {
+        setDrop(false);
+        e.preventDefault();
+      }}
+      onDrop={(e) => {
+        setDrop(false);
+        moveTask(draggedTask, state);
+        setDraggedTask(null);
+      }}
+    >
       <div className="titleWrapper">
         <p>{state}</p>
         <button onClick={() => setOpen(true)}>Add</button>
--- src/store.js
+++ src/store.js
@@ -2,11 +2,19 @@ import { create } from "zustand";
 
 const store = (set) => ({
   tasks: [{ title: "TestTask", state: "DONE" }],
+  draggedTask: null,
   addTask: (title, state) =>
     set((store) => ({ tasks: [...store.tasks, { title, state }] })),
   deleteTask: (title) =>
     set((store) => ({
       tasks: store.tasks.filter((task) => task.title !== title),
+    })),
+  setDraggedTask: (title) => set({ draggedTask: title }),
+  moveTask: (title, state) =>
+    set((store) => ({
+      tasks: store.tasks.map((task) =>
+        task.title === title ? { title, state } : task
+      ),
     })),
 });
 
