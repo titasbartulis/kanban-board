08fd7e1ac2974aeb0a255eb95d2515e9815ab23b
T1t45 <titasbartulis@gmail.com>
2023-12-14 21:45:06 +0200

    Using Zustand Middleware

--- src/store.js
+++ src/store.js
@@ -1,10 +1,15 @@
 import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
 
 const store = (set) => ({
-  tasks: [{ title: "TestTask", state: "DONE" }],
+  tasks: [],
   draggedTask: null,
   addTask: (title, state) =>
-    set((store) => ({ tasks: [...store.tasks, { title, state }] })),
+    set(
+      (store) => ({ tasks: [...store.tasks, { title, state }] }),
+      false,
+      "addTask"
+    ),
   deleteTask: (title) =>
     set((store) => ({
       tasks: store.tasks.filter((task) => task.title !== title),
@@ -18,4 +23,13 @@ const store = (set) => ({
     })),
 });
 
-export const useStore = create(store);
+const log = (config) => (set, get, api) => config(
+  (...args) => {
+    console.log(args);
+    set(...args)
+  },
+  get,
+  api
+)
+
+export const useStore = create(log(persist(devtools(store), {name: "store"})));
