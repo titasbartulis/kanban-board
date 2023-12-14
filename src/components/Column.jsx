40beffc0b761e959670a8d83133515b669800962
T1t45 <titasbartulis@gmail.com>
2023-12-14 21:08:06 +0200

    Implemented the adding of the tasks

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,14 +1,44 @@
-import './Column.css';
-import { useStore } from '../store';
-import Task from './Task';
-import { shallow } from 'zustand/shallow';
+import "./Column.css";
+import { useState } from "react";
+import { useStore } from "../store";
+import Task from "./Task";
+import { shallow } from "zustand/shallow";
 
 export default function Column({ state }) {
-  const tasks = useStore(store => store.tasks.filter(task => task.state === state), shallow);
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const tasks = useStore(
+    (store) => store.tasks.filter((task) => task.state === state),
+    shallow
+  );
+  const addTask = useStore((store) => store.addTask);
+
   return (
-    <div className='column'>
-      <p>{state}</p>
-      {tasks.map((task)=><Task title={task.title} key={task.title} />)}
+    <div className="column">
+      <div className="titleWrapper">
+        <p>{state}</p>
+        <button onClick={() => setOpen(true)}>Add</button>
+      </div>
+      {tasks.map((task) => (
+        <Task title={task.title} key={task.title} />
+      ))}
+      {open && (
+        <div className="Modal">
+          <div className="modalContent">
+            <input onChange={(e) => setText(e.target.value)} value={text} />
+            <button
+              onClick={() => {
+                addTask(text, state);
+                setText("");
+                setOpen(false);
+              }}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 const store = (set) => ({
-  tasks: [{title: 'TestTask', state: 'DONE'}],
+  tasks: [{ title: "TestTask", state: "DONE" }],
+  addTask: (title, state) =>
+    set((store) => ({ tasks: [...store.tasks, { title, state }] })),
 });
 
 export const useStore = create(store);
