1009ef8a827f77256a1a9b1eabec0bb1168cdb96
T1t45 <titasbartulis@gmail.com>
2023-12-14 21:14:45 +0200

    Implemented the deletion of a task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,16 +1,22 @@
 import "./Task.css";
 import classNames from "classnames";
 import { useStore } from "../store";
+import trash from "../assets/trash-2.svg";
 
 export default function Task({ title }) {
   const task = useStore((store) =>
     store.tasks.find((task) => task.title === title)
   );
+
+  const deleteTask = useStore(store=>store.deleteTask);
+
   return (
     <div className="task">
       <div>{task.title}</div>
       <div className="bottomWrapper">
-        <div></div>
+        <div><img src={trash} onClick={() => {
+          deleteTask(task.title)
+        }}/></div>
         <div className={classNames("status", task.state)}>{task.state}</div>
       </div>
     </div>
--- src/store.js
+++ src/store.js
@@ -4,6 +4,10 @@ const store = (set) => ({
   tasks: [{ title: "TestTask", state: "DONE" }],
   addTask: (title, state) =>
     set((store) => ({ tasks: [...store.tasks, { title, state }] })),
+  deleteTask: (title) =>
+    set((store) => ({
+      tasks: store.tasks.filter((task) => task.title !== title),
+    })),
 });
 
 export const useStore = create(store);
